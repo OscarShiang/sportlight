2a7bc5d2f45a35260035102613a81598ae498a2d
huanjing1218 <huanjing881218@gmail.com>
2021-01-10 16:54:19 +0800

    Add sport image

--- app/src/main/java/com/example/sportlight/Adapter.java
+++ app/src/main/java/com/example/sportlight/Adapter.java
@@ -20,11 +20,13 @@ public class Adapter extends RecyclerView.Adapter<Adapter.viewHolder> {
 
     public static class viewHolder extends RecyclerView.ViewHolder {
         public TextView sport, time;
+        public ImageView icon;
 
         public viewHolder(@NonNull View itemView) {
             super(itemView);
             sport = itemView.findViewById(R.id.sport);
             time = itemView.findViewById(R.id.time);
+            icon = itemView.findViewById(R.id.icon);
         }
     }
 
@@ -42,6 +44,32 @@ public class Adapter extends RecyclerView.Adapter<Adapter.viewHolder> {
         SportEvent sport = sportList.get(position);
         holder.sport.setText(sport.getSport());
         holder.time.setText(sport.getStartAt() + "\n" + sport.getFounder());
+        switch(sport.getSport()) {
+            case "游泳":
+                holder.icon.setImageResource(R.drawable.swimming);
+                break;
+            case "健走":
+                holder.icon.setImageResource(R.drawable.walking);
+                break;
+            case "爬山":
+                holder.icon.setImageResource(R.drawable.mountain);
+                break;
+            case "騎腳踏車":
+                holder.icon.setImageResource(R.drawable.biking);
+                break;
+            case "跑步":
+                holder.icon.setImageResource(R.drawable.running);
+                break;
+            case "羽球":
+                holder.icon.setImageResource(R.drawable.badminton);
+                break;
+            case "桌球":
+                holder.icon.setImageResource(R.drawable.tabletennis);
+                break;
+            case "太極拳":
+                holder.icon.setImageResource(R.drawable.taichichuan);
+                break;
+        }
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
